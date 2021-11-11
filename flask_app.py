e01670723463fd1b5dd74f36f6d694bdee522253
user.email <PantakanBunleang@4b5d82362e5e>
2021-11-11 11:10:36 +0000

    latest

--- flask_app.py
+++ flask_app.py
@@ -12,7 +12,7 @@ def hello_world():
 
 @app.route('/apple') #when there is path /apple at the back of the domain name, this method will be executed.
 def hello_apple():
-    return 'This is apple page in the flask webapp.'
+    return 'Thisssss is apple page in the flask webapp.'
 
 @app.route('/today')
 def today():
