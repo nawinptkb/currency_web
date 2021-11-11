07ac0263355cc3a9a8ee94e45c05dec142098b17
user.email <PantakanBunleang@4b5d82362e5e>
2021-11-11 10:58:12 +0000

    First commit

---
+++ flask_app.py
@@ -0,0 +1,19 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+from datetime import date
+
+app = Flask(__name__)
+
+@app.route('/') #Path at the back of the domain name
+def hello_world():
+    return 'Hello, this is Pantakan Bunleang (Nawin).'
+
+@app.route('/apple') #when there is path /apple at the back of the domain name, this method will be executed.
+def hello_apple():
+    return 'This is apple page in the flask webapp.'
+
+@app.route('/today')
+def today():
+    return str(date.today())
